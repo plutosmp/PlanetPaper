e5f0875feb6e1fb24142fa5027b63aed02f5f9a5
nostalgic853 <yuu8583@proton.me>
2022-11-20 14:19:15 +0800

    refactor: 基于 KeYi 重新构建。

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,61 +1,81 @@
-import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
+import io.papermc.paperweight.util.constants.*
 
 plugins {
     java
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.8"
+    id("io.papermc.paperweight.patcher") version "1.3.9"
 }
+
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+    maven(paperMavenPublicUrl) {
+        content { onlyForConfigurations(configurations.paperclip.name) }
     }
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
-    decompiler("net.minecraftforge:forgeflower:1.5.605.7")
-    paperclip("io.papermc:paperclip:3.0.2")
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    decompiler("org.quiltmc:quiltflower:1.8.1")
+    paperclip("io.papermc:paperclip:3.0.3-SNAPSHOT")
 }
 
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "maven-publish")
 
     java {
-        toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
     }
 
-    tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
     }
 
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
     repositories {
-        mavenLocal()
         mavenCentral()
+        maven(paperMavenPublicUrl)
         maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
         maven("https://hub.spigotmc.org/nexus/content/groups/public/")
         maven("https://jitpack.io")
+        maven("https://repo.codemc.io/repository/maven-public/")
     }
+
 }
 
 paperweight {
     serverProject.set(project(":planetpaper-server"))
 
     remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+    decompileRepo.set("https://maven.quiltmc.org/")
 
-    usePaperUpstream(providers.gradleProperty("paperRef")) {
-        withPaperPatcher {
+    useStandardUpstream("keyi") {
+        url.set(github("KeYiMC", "KeYi"))
+        ref.set(providers.gradleProperty("keyiRef"))
+
+        withStandardPatcher {
+            baseName("KeYi")
+
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
-
             apiOutputDir.set(layout.projectDirectory.dir("PlanetPaper-API"))
+
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("PlanetPaper-Server"))
         }
     }
